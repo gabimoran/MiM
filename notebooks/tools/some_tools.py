47e3f37760689e6f32a57c99c63ea9146a3b1e5b
gmoran <morangabriela22@gmail.com>
2022-03-24 13:47:39 -0300

    Funciones empaquetadas para creacion de carpetas y de prepocesamiento.

---
+++ notebooks/tools/some_tools.py
@@ -0,0 +1,110 @@
+from os import path, listdir, makedirs
+import numpy as np
+from matplotlib import pyplot as plt
+
+from skimage import filters, measure, img_as_ubyte
+from scipy.ndimage.morphology import binary_fill_holes
+from skimage.transform import rescale
+
+
+		
+def folder_path(dic, rute = ''): 
+    ''' From a dict with the folders scheme
+        returns all the possible paths'''
+    rutes = []
+    if not isinstance(dic, dict) or not dic: 
+        return rute
+    else:
+        for key in dic:
+            aux = folder_path(dic[key], rute = rute+key)
+            if isinstance(aux,list):
+                rutes += aux
+            else:
+                rutes.append(aux)
+    return rutes
+
+	
+def create_rute(rute):
+    ''' Creates rute if not exists.
+        rute: path to create.'''
+    if path.exists(rute):
+        print(f'Already exists: "{rute}".')
+    else:
+        try:
+            makedirs(rute)
+            print(f'Created successfully: "{rute}".')
+        except:
+            print(f'Could not create: "{rute}".')		
+
+def create_folders_scheme(scheme):
+	''' Creates folders scheme.
+		scheme: dict. Scheme of folders to create.
+	'''
+	for rute in folder_path(scheme):
+		create_rute(rute)
+
+		
+#----------------------------------------------------------------------------------------#
+#                            Preprocessing functions                                      #
+#----------------------------------------------------------------------------------------#
+
+def get_fov_mask(fundus_picture):
+    '''
+    Obtains the fov mask of the given fundus picture.
+    '''
+
+    # sum the R, G, B channels to form a single image
+    sum_of_channels = np.asarray(np.sum(fundus_picture,axis=2), dtype=np.uint8)
+    # threshold the image using Otsu
+    fov_mask = sum_of_channels > filters.threshold_otsu(sum_of_channels)
+    # fill holes in the approximate FOV mask
+    fov_mask = np.asarray(binary_fill_holes(fov_mask), dtype=np.uint8)
+
+    return fov_mask
+	
+
+def crop_fov(fundus_picture):
+    '''
+    Extract an approximate FOV mask, and crop the picture around it
+    '''
+
+    #get the fov mask of the picture
+    fov_mask = get_fov_mask(fundus_picture)
+
+    # get the coordinate of a bounding box around the fov mask
+    coordinates = measure.regionprops(fov_mask)[0].bbox
+
+    # crop the image and return
+    return fundus_picture[coordinates[0]:coordinates[2],coordinates[1]:coordinates[3],:]
+
+def crop_fov_pill(fundus_picture):
+    '''
+    Extract an approximate FOV mask, and crop the picture around it
+    '''
+
+    #get the fov mask of the picture
+    fov_mask = get_fov_mask(fundus_picture)
+
+    # get the coordinate of a bounding box around the fov mask
+    coordinates = measure.regionprops(fov_mask)[0].bbox
+
+    # crop the image and return
+    return fundus_picture.crop((coordinates[1],coordinates[0],coordinates[3],coordinates[2]))
+
+def downsize_image(fundus_picture, image_size=[512, 512]):
+    '''
+    Downsize the input image to the target resolution
+    '''
+
+    # get the proper size
+    if fundus_picture.size[0] >= fundus_picture.size[1]:
+        factor = image_size[0] / fundus_picture.size[0]
+    else:
+        factor = image_size[0] / fundus_picture.size[1]
+    
+    target_size = (round(fundus_picture.size[0] * factor), round(fundus_picture.size[1] * factor))
+
+    # apply the transformation
+    resized_fundus_picture = fundus_picture.resize(size=target_size)
+
+    return resized_fundus_picture
