42e01cc7c9131d5e9f4a66ab463bd613efbd0391
gabimoran <morangabriela22@gmail.com>
2025-03-02 09:36:33 -0300

    cambios en el preprocesamiento. Cambie PIL por OpenCV y agregue una mascara circular

--- notebooks/tools/some_tools.py
+++ notebooks/tools/some_tools.py
@@ -2,9 +2,9 @@ from os import path
 import numpy as np
 from skimage import filters, measure
 from scipy.ndimage import binary_fill_holes
-from PIL import Image, ImageFile
+# from PIL import Image, ImageFile
+import cv2
 import multiprocessing as mp 
-ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 
 		
@@ -53,17 +53,39 @@ def get_fov_mask(fundus_picture):
     Obtains the fov mask of the given fundus picture.
     '''
 
-    # sum the R, G, B channels to form a single image
-    # sum_of_channels = np.asarray(np.sum(fundus_picture,axis=2), dtype=np.uint8)
-    sum_of_channels = np.sum(fundus_picture,axis=2, dtype=np.uint8)
-    # threshold the image using Otsu
-    fov_mask = sum_of_channels > filters.threshold_otsu(sum_of_channels)
-    # fill holes in the approximate FOV mask
-    fov_mask = np.asarray(binary_fill_holes(fov_mask), dtype=np.uint8)
+    # try to detect a cicle
+    gray = cv2.cvtColor(fundus_picture, cv2.COLOR_BGR2GRAY)
+    gray_blur = cv2.medianBlur(gray, 7)
+
+    rows = gray_blur.shape[0]
+    cols = gray_blur.shape[1]
+    circles = cv2.HoughCircles(gray_blur,
+                               method=cv2.HOUGH_GRADIENT,
+                               dp=1,
+                               minDist=cols//2,
+                               param1=60,
+                               param2=30,
+                               minRadius=min(rows,cols)//4,
+                               maxRadius=max(rows,cols)//2)
+
+    if circles is not None and circles.shape[1] == 1:
+        circles = np.uint16(np.around(circles))
+        circle = circles[0,0]
+        fov_mask = np.zeros((rows,cols),np.uint16)
+        center = (circle[0], circle[1])
+        radius = circle[2]
+        cv2.circle(fov_mask, center, radius, 255 , -1)
+
+    else:
+        sum_of_channels = np.sum(fundus_picture,axis=2)
+        threshold_fondo = filters.threshold_otsu(sum_of_channels.astype(np.int64))
+        fov_mask = sum_of_channels > threshold_fondo
+        fov_mask = binary_fill_holes(fov_mask)
+
     return fov_mask
 	
 
-def crop_fov(fundus_picture):
+def crop_fov(fundus_picture, fov_mask):
     '''
     Extract an approximate FOV mask, and crop the picture around it
     '''
@@ -71,25 +93,34 @@ def crop_fov(fundus_picture):
     #get the fov mask of the picture
     fov_mask = get_fov_mask(fundus_picture)
 
+    fov_mask = np.asarray(fov_mask, dtype=np.uint8)
+    
     # get the coordinate of a bounding box around the fov mask
     coordinates = measure.regionprops(fov_mask)[0].bbox
+
+    # Set to black every pixel outside the fov_mask
+    fundus_picture[fov_mask<=0,...]=0
 
     # crop the image and return
     return fundus_picture[coordinates[0]:coordinates[2],coordinates[1]:coordinates[3],:]
 
-def crop_fov_pill(fundus_picture):
+def set_black_background(fundus_picture):
     '''
-    Extract an approximate FOV mask, and crop the picture around it
+    Set background to black if it is not already
     '''
 
-    #get the fov mask of the picture
-    fov_mask = get_fov_mask(fundus_picture)
+    gray = cv2.cvtColor(fundus_picture, cv2.COLOR_BGR2GRAY)
+    freq_intensity = np.bincount(gray.ravel().astype(np.int64))
 
-    # get the coordinate of a bounding box around the fov mask
-    coordinates = measure.regionprops(fov_mask)[0].bbox
+    # If mode is 255, then it means the background is white
+    if np.where(freq_intensity == freq_intensity.max())[0].item() == 255:
+        index = np.where(gray == 255)
+        imagen_modified = fundus_picture.copy()
+        imagen_modified[index[0],index[1],...] = 0
+    else:
+        imagen_modified = fundus_picture.copy()
 
-    # crop the image and return
-    return fundus_picture.crop((coordinates[1],coordinates[0],coordinates[3],coordinates[2]))
+    return imagen_modified
 
 
 def preprocess_and_save(pd_labels, save_to, image_size=[512, 512]):
@@ -98,11 +129,13 @@ def preprocess_and_save(pd_labels, save_to, image_size=[512, 512]):
     '''
     for image_dir in pd_labels.file_path:
         try:
-            new_name = path.split(image_dir.with_suffix('.png'))[-1]
-            im = Image.open(image_dir)       
-            im_fov = crop_fov_pill(im)
-            im_fov = im_fov.resize(image_size)
-            im_fov.save(save_to / new_name)
+            new_name = image_dir.with_suffix('.png').name
+            im = cv2.imread(image_dir)
+            im_new = set_black_background(im)
+            fov_mask = get_fov_mask(im_new)
+            im_new = crop_fov(im_new,fov_mask)
+            im_new = cv2.resize(im_new, (512, 512))
+            cv2.imwrite(str(save_to / new_name), im_new)  
         except Exception as e:
             print(f"Error processing {image_dir}: {e}")
     return
